2f1c878e5efd68c8948d1247f412583c8af60148
MahfuzNazib <nazibmahfuz60@gmail.com>
2022-12-10 18:34:42 +0600

    First Commit with First C# Code

---
+++ InterfaceImplementation/InterfaceImplementation/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace InterfaceImplementation
+{
+    internal class Program
+    {
+        public interface IAreaCalculation
+        {
+            void AreaResult();
+            void GetShapeSize();
+        }
+
+        public interface ITexCalculation
+        {
+            void AreaResult();
+        }
+
+        public class Rectengle : IAreaCalculation, ITexCalculation
+        {
+            public void AreaResult()
+            {
+                Console.WriteLine("Area of Rectengle is : 20");
+            }
+
+            public void GetShapeSize()
+            {
+                Console.WriteLine("The shpae of the size is : Rectengle");
+            }
+        }
+
+
+        public class Circle : IAreaCalculation, ITexCalculation
+        {
+            void IAreaCalculation.AreaResult()
+            {
+                Console.WriteLine("Area of Circle is : 99.009");
+            }
+
+            void ITexCalculation.AreaResult()
+            {
+                Console.WriteLine("Area Result from TexCalculation");
+            }
+
+            public void GetShapeSize()
+            {
+                Console.WriteLine("The shape of the size is : Circle");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Rectengle rectengle = new Rectengle();
+            rectengle.AreaResult();
+            rectengle.GetShapeSize();
+
+            Circle circle = new Circle();
+            
+            IAreaCalculation iArea = new Circle();
+            iArea.AreaResult();
+
+            ITexCalculation iTex = new Circle();
+            iTex.AreaResult();
+
+            circle.GetShapeSize();
+        }
+    }
+}
---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Nazib
+{
+    
+    public static void Main()
+    {
+        int[] nums = { 2, 7, 11, 9 };
+        int targetNumber = 9;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] + nums[i+1] == targetNumber)
+            {
+                Console.WriteLine("[" + i + "," + (i+1) + "]");
+                break;
+            }
+        }
+    }
+
+    
+}
+
