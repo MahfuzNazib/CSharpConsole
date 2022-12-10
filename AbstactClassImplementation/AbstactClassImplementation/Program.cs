18a3d05a21d3ae288d3beab58dcc7718df01b519
MahfuzNazib <nazibmahfuz60@gmail.com>
2022-12-10 19:22:55 +0600

    Add Abstract Class

---
+++ AbstactClassImplementation/AbstactClassImplementation/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace AbstactClassImplementation
+{
+    internal class Program
+    {
+        /*
+         * Define an Abstract Class called Shape. In this Shape abstract class there has a abstract method called Draw.
+         * This Draw() method have no body. 
+         */
+        public abstract class Shape
+        {
+            public abstract void Draw();
+        }
+
+        /*
+         * A New class called Rectengle which inherite Shape abstract class.
+         */
+        public class Rectengle : Shape
+        {
+            /* This Draw class must be overrided. Because its a abstract method. 
+             * Abstract method can't use without overriding technique.
+            */
+            public override void Draw()
+            {
+                Console.WriteLine("Draw from Rectengle Class");
+            }
+        }
+
+        public class Circle : Rectengle, CalculateSize
+        {
+            public int SumTwo()
+            {
+                return 10 + 25;
+            }
+            
+            public override void SizeOfBox()
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Rectengle rectengle = new Rectengle();
+            rectengle.Draw();
+
+            Circle circle = new Circle();
+            Console.WriteLine(circle.SumTwo());
+            circle.Draw();
+        }
+    }
+}
--- InterfaceImplementation/InterfaceImplementation/Program.cs
+++ InterfaceImplementation/InterfaceImplementation/Program.cs
@@ -24,7 +24,7 @@
 
         /*
          * New class called Rectengle. This class inherite one interfaces
-         * which is IAreaCalculation. In that case Both two methods must be declare inside Rectengle class
+         * which is IAreaCalculation. In that case Both two methods must be declare inside Rectengle class.
          */
         public class Rectengle : IAreaCalculation
         {
@@ -39,19 +39,29 @@
             }
         }
 
-
+        /*
+         * Another new class called Circle. This class inherit two Interface class. 
+         * 1. IAreaCalculation & 
+         * 2. ITexCalculation.
+         * 
+         * Here one thing is noticable. Bothe Interface have a same name method called AreaResult()
+         * Here we need explicite interface implementation.
+         */
         public class Circle : IAreaCalculation, ITexCalculation
         {
+            // Explicite Implementation. Called IAreaCalculation AreaResult Method
             void IAreaCalculation.AreaResult()
             {
                 Console.WriteLine("Area of Circle is : 99.009");
             }
 
+            // Explicite Implementation. Called ITexCalculation AreaResult Method
             void ITexCalculation.AreaResult()
             {
                 Console.WriteLine("Area Result from TexCalculation");
             }
 
+            // Just Called the method. Because its a unique method from IAreaCalculation Interface.
             public void GetShapeSize()
             {
                 Console.WriteLine("The shape of the size is : Circle");
@@ -60,10 +70,16 @@
 
         static void Main(string[] args)
         {
+            // Create a new Object of Rectengle Class & called classes methods.
             Rectengle rectengle = new Rectengle();
             rectengle.AreaResult();
             rectengle.GetShapeSize();
 
+            /*
+             * Create a new Object of Circle Class. But here we can't call circle.AreaResult() method like this.
+             * Because we have two same method from different Interface Classes.
+             * That's why we called it like below 
+             */
             Circle circle = new Circle();
             
             IAreaCalculation iArea = new Circle();
