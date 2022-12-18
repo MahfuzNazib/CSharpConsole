62ab9b0f1af4e064c6882309cbbad3b26e9fc9c5
MahfuzNazib <nazibmahfuz60@gmail.com>
2022-12-18 18:06:12 +0600

    Some More C# Concept

---
+++ AsyncAwaitThread/AsyncAwaitThread/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AsyncAwaitThread
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            LongProcess();
+            ShortProcess();
+        }
+
+        public static async Task LongProcess()
+        {
+            Console.WriteLine("Long Process Start");
+            Console.WriteLine("==Waiting 4 sec.==");
+            await Task.Delay(4000);
+            Console.WriteLine("Long Process End!!");
+        }
+
+        public static async Task ShortProcess()
+        {
+            Console.WriteLine("===================");
+            Console.WriteLine("Short Process Start");
+            Console.WriteLine("No Waiting");
+            Console.WriteLine("Short Process End");
+        }
+    }
+}
---
+++ StringReverseWithRecursion/StringReverseWithRecursion/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace StringReverseWithRecursion
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            string[] operations = { "X++", "++X", "--X", "X--" };
+            int result = 0;
+
+            for(int i = 0; i< operations.Length; i++)
+            {
+                if (operations[i].Contains("--X"))
+                {
+                    result -= 1;
+                }
+                else if (operations[i].Contains("X--"))
+                {
+                    result -= 1;
+                }
+                else if(operations[i].Contains("X++"))
+                {
+                    result += 1;
+                }else
+                {
+                    result += 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
