148ab5dfdc7470d1c373a8748a9b8eadfd295a2e
MahfuzNazib <nazibmahfuz60@gmail.com>
2023-03-15 18:10:40 +0600

    Delegates Implements

---
+++ CSharpDelegates/CSharpDelegates/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharpDelegates
+{
+    //Define Delegates
+    public delegate void PerformCalculation(double param1, double param2);
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectengle rectengle= new Rectengle();
+            rectengle.RectengleAre(1.25, 3.52);
+            rectengle.PairRectengle(25.24, 8.3);
+        }
+    }
+}
---
+++ CSharpDelegates/CSharpDelegates/Rectengle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDelegates
+{
+    public class Rectengle
+    {
+        public void RectengleAre(double height, double width)
+        {
+            Console.WriteLine("Area is : " + height * width);
+        }
+
+        public void PairRectengle(double feet, double inch)
+        {
+            Console.WriteLine("Pair is : ", 2*(feet + 110) - inch);
+        }
+    }
+}
